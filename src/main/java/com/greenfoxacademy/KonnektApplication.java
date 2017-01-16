7ff096442830187503d6c504dd95612ec332fcfc
Pósa Márton <posa.marci@gmail.com>
2017-01-16 11:18:38 +0100

    initial project commit

---
+++ src/main/java/com/greenfoxacademy/KonnektApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonnektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonnektApplication.class, args);
+	}
+}
