eece1b9c7b868bac07acabac1f13c5e006a53964
Pósa Márton <posa.marci@gmail.com>
2017-01-16 12:00:11 +0100

    add hello controller from heroku guide

--- src/main/java/com/greenfoxacademy/KonnektApplication.java
+++ src/main/java/com/greenfoxacademy/KonnektApplication.java
@@ -2,9 +2,20 @@ package com.greenfoxacademy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 @SpringBootApplication
 public class KonnektApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KonnektApplication.class, args);
