6822dd82a1fb603157dcb80c821cb130e1908819
bsoptei <bsoptei@gmail.com>
2017-01-19 13:54:56 +0100

    flyway config is java based, db password comes from environmental var

--- src/main/java/com/greenfoxacademy/config/KonnektAppConfig.java
+++ src/main/java/com/greenfoxacademy/config/KonnektAppConfig.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.config;
 
+import org.flywaydb.core.Flyway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -15,11 +16,21 @@ public class KonnektAppConfig {
                 new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("org.postgresql.jdbc3.Jdbc3ConnectionPool");
         driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/");
-        // Configure user name and password for database connection
+        // Configure user name
         driverManagerDataSource.setUsername("postgres");
-        driverManagerDataSource.setPassword("admin1234");
-        //
-
+        // Obtain password from environmental variable
+        driverManagerDataSource.setPassword(System.getenv("DB_PASSWORD"));
         return driverManagerDataSource;
     }
+
+    @Bean(initMethod = "migrate")
+    Flyway flyway() {
+        Flyway flyway = new Flyway();
+        flyway.setBaselineOnMigrate(true);
+        flyway.setSchemas("konnekt");
+        flyway.setLocations("filesystem:/src/main/java/com/greenfoxacademy/db/migration");
+        flyway.setDataSource(dataSource());
+        return flyway;
+    }
+
 }
