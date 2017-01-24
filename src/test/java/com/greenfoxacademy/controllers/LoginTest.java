351fdfaa82fc64daba1bc5c45ba34cdb87bc4c97
Rita Pasztor <pasztorrita@gmail.com>
2017-01-24 11:57:15 +0100

    working unit test for login with in memory authentication

--- src/test/java/com/greenfoxacademy/controllers/LoginTest.java
+++ src/test/java/com/greenfoxacademy/controllers/LoginTest.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.controllers;
 
-import com.greenfoxacademy.config.KonnektAppConfig;
 import com.greenfoxacademy.config.Profiles;
-import com.greenfoxacademy.config.SecurityWebAppInitializer;
-import com.greenfoxacademy.config.WebSecurityConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,16 +16,19 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.servlet.Filter;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {SecurityWebAppInitializer.class, WebSecurityConfig.class, KonnektAppConfig.class})
+@ContextConfiguration
 @WebAppConfiguration
 @ActiveProfiles(Profiles.DEV)
 public class LoginTest {
@@ -49,9 +53,28 @@ public class LoginTest {
     @Test
     public void thatLoginWithCorrectCredentialsWorks() throws Exception {
         mockMvc.perform(post("/login").with(csrf())
-                        .param("username", "admin")
-                        .param("password", "admin")
-        ).andExpect(status().isOk());
+                .param("username", "user")
+                .param("password", "password")
+        ).andExpect(status().isFound()).andExpect(authenticated());
     }
 
+    @Test
+    public void thatLoginWithInCorrectCredentialsWorks() throws Exception {
+        mockMvc.perform(post("/login").with(csrf())
+                .param("username", "user")
+                .param("password", "pass3243word")
+        ).andExpect(status().isFound()).andExpect(unauthenticated());
+    }
+
+    @Configuration
+    @EnableWebMvcSecurity
+    @EnableWebMvc
+    static class Config extends WebSecurityConfigurerAdapter {
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
+    }
 }
