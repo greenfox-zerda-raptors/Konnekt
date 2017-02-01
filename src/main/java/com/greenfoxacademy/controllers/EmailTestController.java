111706f29b6701c8426515ba9824327e481119d2
Pósa Márton <posa.marci@gmail.com>
2017-02-01 14:31:27 +0100

    remove unused mysql dependencies, create sendgrid testing classes

---
+++ src/main/java/com/greenfoxacademy/controllers/EmailTestController.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.controllers;
+
+/**
+ * Created by posam on 2017-02-01.
+ * WHAAAAAAAAAAAAAAAASSSSSUUUUUP
+ */
+public class EmailTestController {
+}
---
+++ src/main/java/com/greenfoxacademy/service/EmailTest.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.service;
+
+/**
+ * Created by posam on 2017-02-01.
+ * WHAAAAAAAAAAAAAAAASSSSSUUUUUP
+ */
+public class EmailReminder {
+
+    final String senderAddress = "konnekt@konnekt.hu";
+
+
+    public EmailReminder() {
+
+    }
+
+    public void sendReminder(String userEmail) {
+
+    }
+}
