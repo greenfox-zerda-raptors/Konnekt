d7ef34cc8686a209fe6ad5f9a68ff3641c9202a6
Pósa Márton <posa.marci@gmail.com>
2017-02-01 14:39:55 +0100

    forgot to stage last changes

--- src/main/java/com/greenfoxacademy/service/EmailTest.java
+++ src/main/java/com/greenfoxacademy/service/EmailTest.java
@@ -1,19 +1,34 @@
 package com.greenfoxacademy.service;
 
-/**
- * Created by posam on 2017-02-01.
- * WHAAAAAAAAAAAAAAAASSSSSUUUUUP
- */
-public class EmailReminder {
+import com.sendgrid.*;
+import org.springframework.stereotype.Service;
 
-    final String senderAddress = "konnekt@konnekt.hu";
+import java.io.IOException;
+
+@Service
+public class EmailTest {
+    private Email from = new Email("app62245770@heroku.com");
 
 
-    public EmailReminder() {
+    public void sendTest(String toAddress) {
+        String subject = "Hello World from the SendGrid Java Library!";
+        Email to = new Email(toAddress);
+        Content content = new Content("text/plain", "Hello, Email!");
+        Mail mail = new Mail(from, subject, to, content);
 
-    }
-
-    public void sendReminder(String userEmail) {
-
+        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+        Request request = new Request();
+        try {
+            request.method = Method.POST;
+            request.endpoint = "mail/send";
+            request.body = mail.build();
+            Response response = sg.api(request);
+            System.out.println(response.statusCode);
+            System.out.println(response.body);
+            System.out.println(response.headers);
+        } catch (IOException ex) {
+            System.out.println("juj");
+            ex.printStackTrace();
+        }
     }
 }
