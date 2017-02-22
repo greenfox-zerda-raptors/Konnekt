38c4de8752a2fab6d27b9afef842dc178e38d2a1
Pósa Márton <posa.marci@gmail.com>
2017-02-22 15:45:53 +0100

    renamed various methods, objects, switched out registrationisvalid for new method

---
+++ src/main/java/com/greenfoxacademy/constants/Valid.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.constants;
+
+/**
+ * Created by posam on 2017-02-22.
+ * WHAAAAAAAAAAAAAAAASSSSSUUUUUP
+ */
+public class Valid {
+}
---
+++ src/main/java/com/greenfoxacademy/responses/Action.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.responses;
+
+/**
+ * Created by posam on 2017-02-22.
+ * WHAAAAAAAAAAAAAAAASSSSSUUUUUP
+ */
+public class Action {
+}
